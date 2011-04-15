d00c0c81580ee95f5d9f7d8b86be78792fd70b34
esj <edmundsjackson@gmail.com>
2011-04-15 12:23:35 +0100

    initial import

---
+++ src/main/clojure/inference/star_gazer.clj
@@ -0,0 +1,34 @@
+(ns inference.star-gazer
+  (:use [incanter.core    :as i]
+	[incanter.charts :as c]))
+
+;; A starfield is an sorted set of numbers between 0 and 1 representing
+;; the x position of the star.
+(defn star-field [n]
+     (apply sorted-set (take n (repeatedly rand))))
+
+;; Return all the stars to the left of randomly placed wall.
+(defn obscure-field [field]
+  (let [wall-pos (rand)]
+    {:wall  wall-pos
+     :field (take-while #(<= % wall-pos) field)}))
+
+;; Measure the gap between the last star and the wall.  Account for cases
+;; where wall < field
+(defn gap [field]
+  (if-let [last-star (-> field :field last)]
+    (- (:wall field) last-star)))
+
+;; Perform the experiment.  Filter is for the nils when wall < last_star
+(defn gap-stats [num-stars num-samples]
+  (filter identity
+	  (take num-samples
+		(repeatedly
+		 (fn []
+		   (gap (obscure-field (star-field num-stars))))))))
+
+;; Show me the money, honey.
+(i/view (c/histogram (gap-stats 100 100000)
+		     :nbins 500
+		     :density true
+		     :title "Final Gap Length"))
